ffbebceebd8af5d7b95c314173328f5a00f47e37
endersonlg <endersonluciog@gmail.com>
2023-05-30 11:09:06 -0300

    finished third class

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias maxime tempora perferendis quia, nobis soluta neque fugiat animi excepturi est. Dignissimos provident, ut nostrum libero tempore sit! Sint, sit nam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
