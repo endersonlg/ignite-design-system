1d2eb02b9cac671ab6ccdeb6021ff5f3acc42352
endersonlg <endersonluciog@gmail.com>
2023-05-31 15:25:02 -0300

    test

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal:(config,{configType})=>:{
+    if(configType==='PRODUCTION'){
+      config.base = '/ignite-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
