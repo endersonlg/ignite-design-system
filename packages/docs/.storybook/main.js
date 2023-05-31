bc23f443f9ec86dd5fe1701ae8e4ef436296f7e7
endersonlg <endersonluciog@gmail.com>
2023-05-31 16:16:13 -0300

    fix error

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,12 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
-  viteFinal:(config,{configType})=>:{
-    if(configType==='PRODUCTION'){
-      config.base = '/ignite-design-system/'
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-design-system/";
     }
 
-    return config
-  }
+    return config;
+  },
 };
 export default config;
