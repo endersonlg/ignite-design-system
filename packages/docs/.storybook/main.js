78949f59dd4c400f65765d29a7763f0a4d863e5c
endersonlg <endersonluciog@gmail.com>
2023-05-31 21:26:49 -0300

    test

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,18 +1,17 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
+module.exports = {
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  docs: {
-    autodocs: "tag",
+  features: {
+    storyStoreV7: true,
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
@@ -22,4 +21,3 @@ const config = {
     return config;
   },
 };
-export default config;
